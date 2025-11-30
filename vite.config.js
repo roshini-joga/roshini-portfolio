095305579c3d0041dbeeca5cf97a0d276a6d21ba
roshini-joga <71872073+roshini-joga@users.noreply.github.com>
2025-11-29 19:28:52 -0800

    Configure base path and homepage for GH Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: './', // Use relative paths for assets
+    base: '/roshini-portfolio/', // Set base path for GitHub Pages
     build: {
         outDir: 'dist',
     }
